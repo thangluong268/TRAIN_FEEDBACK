79762f4acfc49eba46dfb90593d52cdd2dd80373
Myth <luongthangg268@gmail.com>
2024-05-25 00:33:31 +0700

    fix: add schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { CronjobsService } from 'train/cron-job.service';
 import { TrainModule } from 'train/train.module';
 
 @Module({
-  imports: [TrainModule],
+  imports: [TrainModule, ScheduleModule.forRoot()],
   controllers: [],
   providers: [CronjobsService],
 })
