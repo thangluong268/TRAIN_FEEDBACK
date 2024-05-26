685c5f7d627f0838d9a3c8dec3a351cb8cb84af3
Myth <luongthangg268@gmail.com>
2024-05-27 02:08:52 +0700

    fix

--- src/train/train.service.ts
+++ src/train/train.service.ts
@@ -6,11 +6,8 @@ import { remove, utils_data } from 'utils/text-util.final.util';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { BayesClassifier, AggressiveTokenizerVi } = require('natural');
+import { AggressiveTokenizerVi, BayesClassifier } from 'natural';
 const TOKENIZER = new AggressiveTokenizerVi();
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const natural = require('natural');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fastText = require('fasttext');
@@ -75,7 +72,7 @@ export class TrainService {
 
     let labelNatural = '';
     // Phân loại new feedback của natural
-    natural.BayesClassifier.load(TRAIN_MODEL_FILE_PATH_NATURAL, null, function (err, classifier) {
+    BayesClassifier.load(TRAIN_MODEL_FILE_PATH_NATURAL, null, function (err, classifier) {
       const textAfterTokenize = TOKENIZER.tokenize(finalText);
       labelNatural = classifier.classify(textAfterTokenize);
       console.log(labelNatural);
