160d83ccf7ed55058c0ab6f855941aa79eafb0ad
Myth <luongthangg268@gmail.com>
2024-06-10 16:39:25 +0700

    fix: remove cronjob

--- src/train/cron-job.service.ts
+++ src/train/cron-job.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
 import { TrainService } from './train.service';
 
 @Injectable()
@@ -7,14 +6,14 @@ export class CronjobsService {
   private readonly logger = new Logger(CronjobsService.name);
   constructor(private readonly trainService: TrainService) {}
 
-  @Cron('0 * * * * *')
-  onStart() {
-    this.logger.log('Entry CronJobs Service starting...');
-  }
+  // @Cron('0 * * * * *')
+  // onStart() {
+  //   this.logger.log('Entry CronJobs Service starting...');
+  // }
 
-  @Cron(CronExpression.EVERY_2_HOURS)
-  async trainData() {
-    this.logger.log('Training data...');
-    this.trainService.trainModel();
-  }
+  // // @Cron(CronExpression.EVERY_2_HOURS)
+  // // async trainData() {
+  // //   this.logger.log('Training data...');
+  // //   this.trainService.trainModel();
+  // // }
 }
